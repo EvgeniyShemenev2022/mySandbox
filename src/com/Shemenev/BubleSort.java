64095dd62f5ba0524171dd200a9c90d42d52c087
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-10-31 11:55:38 +0300

    BubleSort

---
+++ src/com/Shemenev/BubleSort.java
@@ -0,0 +1,31 @@
+package com.Shemenev;
+
+import java.util.Arrays;
+
+/**
+ * Сортировка пузырьком не очень эффективный алгоритм,
+ * т.к. используется вложенный цикл.
+ *
+ * Применим лишь для малых наборов значений.
+ * Асимптотическая сложность O(n^2)
+ */
+public class BubleSort {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{11, 37, 1, 2, 4, 46, 94, 16, 24, -4, 8, 8 , 8,  26, 39, 31, 86, 12, 38};
+
+
+        for (int i = 0; i < array.length - 1 - i; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+
+                    array[j] ^= array[j + 1];
+                    array[j + 1] ^= array[j];
+                    array[j] ^= array[j + 1];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
